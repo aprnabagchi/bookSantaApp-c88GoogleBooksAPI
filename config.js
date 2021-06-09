0bd85bb14fb21137066eca66971062923e7e3dc4
aprnabagchi <71764327+aprnabagchi@users.noreply.github.com>
2021-06-09 05:33:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+require('@firebase/firestore')
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  var firebaseConfig = {
+  apiKey: "AIzaSyDGNHMcG6OtOuCNBR7tHs_lx3DXBlFN8IE",
+  authDomain: "booksantaapp-dfe9d.firebaseapp.com",
+  databaseURL: "https://console.firebase.google.com/project/undefined/firestore/data/",
+  projectId: "booksantaapp-dfe9d",
+  storageBucket: "booksantaapp-dfe9d.appspot.com",
+  messagingSenderId: "160399703411",
+  appId: "1:160399703411:web:bc61c2e9ad7320d6ded906"
+};
+
+firebase.initializeApp(firebaseConfig)
+export default firebase.firestore()
